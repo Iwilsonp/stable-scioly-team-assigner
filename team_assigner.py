009cee1086508ae95fb0e040e2dd9377ef668bf4
Iwilsonp <peteriwilson@verizon.net>
2018-01-17 15:11:46 -0500

    with the code file

---
+++ team_assigner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 15:03:13 2018
+
+@author: me
+"""
+import numpy as np
+import os
+import math
+import networkx as nx
+from team_data import *  #so team scores aren't pushed to github
+
+
